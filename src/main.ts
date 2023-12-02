8eb915f1e60e0a30fefce3bdd01ca7ebab5bbeac
12Aleks <tanais2010@yahoo.com>
2023-12-02 18:31:49 +0100

    The session has been added in the project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import * as session from 'express-session';
+import * as passport from 'passport';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.use(
+        session({
+            name: 'Session',
+            secret:  process.env.SESSION_KEY,
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+              maxAge: 60000
+            }
+        })
+    );
+    await app.listen(3000);
 }
+
 bootstrap();
