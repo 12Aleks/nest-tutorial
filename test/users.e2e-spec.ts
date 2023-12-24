e240690b18c5d7bf458cc5494bfbc1991640c70b
12Aleks <tanais2010@yahoo.com>
2023-12-24 14:38:00 +0100

    The user.e2e file has been updated

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -13,15 +13,55 @@ describe('UsersController E2E test', () => {
       }).compile();
 
       app = moduleFixture.createNestApplication();
+      app.setGlobalPrefix('api');
       await app.init();
   })
 
-   it('should create a new user', () => {
-       return request(app.getHttpServer())
-           .post('/api/users/create').send({
-               username: 'Tom',
-               password: '123',
-               email: 'test@gmail.com'
-           })
-   })
+
+    describe('Creating new Users POST /api/users/create', () => {
+        const CREATE_USER_URL = '/api/users/create';
+
+        it('should create a new user', async () => {
+            const response = await request(app.getHttpServer())
+                .post(CREATE_USER_URL).send({
+                    username: 'Tom',
+                    password: '123456789101112',
+                    email: 'best1Test@gmail.com'
+                }).expect(201)
+
+            return response
+        })
+
+        it('should return a 400 when invalid username is provided', () => {
+            return request(app.getHttpServer())
+                .post(CREATE_USER_URL)
+                .send({
+                    username: 'an',
+                    password: '123456789012',
+                    email: 'testnyr@email.com'
+                }).expect(400)
+        })
+
+        it('should return a 400 when invalid password is provided', () => {
+            return request(app.getHttpServer())
+                .post(CREATE_USER_URL)
+                .send({
+                    username: 'Jim',
+                    password: '123456',
+                    email: 'testnyruuu@email.com'
+                }).expect(400)
+        })
+
+        it('should return a 400 when invalid email is provided', () => {
+            return request(app.getHttpServer())
+                .post(CREATE_USER_URL)
+                .send({
+                    username: 'Jim',
+                    password: '123456789456',
+                    email: 'testnyruuu'
+                }).expect(400)
+        })
+    })
+
+
 })
